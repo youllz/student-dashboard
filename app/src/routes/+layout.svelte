5ad75f4451401a02e711547c082a57a0cbef11c0
youllz <youssoufzerbo01@gmail.com>
2023-08-29 08:21:27 +0000

    init

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.scss';
+	import "carbon-components-svelte/css/all.css";
+	import { Theme, Button } from "carbon-components-svelte";
+</script>
+
+
+<Theme theme="g90"/>
+
+<slot />
---
+++ app/svelte.config.js
@@ -0,0 +1,26 @@
+import adapter from '@sveltejs/adapter-auto';
+import { sequence } from '@sveltejs/kit/hooks';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import { optimizeImports } from "carbon-preprocess-svelte";
+import { elements } from "carbon-preprocess-svelte";
+import { icons } from "carbon-preprocess-svelte";
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: sequence( [optimizeImports(), elements(), icons()]),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		// vite: {
+		// 	plugins: [process.env.NODE_ENV === "production" && optimizeCss()],
+		// }
+	}
+};
+
+export default config;
